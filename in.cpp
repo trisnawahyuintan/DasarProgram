0b939f396a0894ca7617572ba296cd351cfe204b
trisnawahyuintan <136420386+trisnawahyuintan@users.noreply.github.com>
2023-06-30 22:17:55 +0700

    tambah void

--- in.cpp
+++ in.cpp
@@ -8,3 +8,11 @@ struct Kamar {
     int harga;
     bool tersedia;
 };
+
+Kamar hotel[10];
+int jumlahkamar=10;
+
+void tampilkanDataKamar(){
+    cout<<"      PROGRAM SEWA KAMAR HOTEL      "<<endl<<endl;
+    for(int i=0;i<jumlahKamar;i++){
+    cout<<"Nomor Kamar : "<<hotel[i].nomorKamar<<endl
