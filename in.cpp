7207ffc180cfa61714e543cc4ddc1947ce17687a
trisnawahyuintan <136420386+trisnawahyuintan@users.noreply.github.com>
2023-07-02 19:32:42 +0700

    lengkapi void

--- in.cpp
+++ in.cpp
@@ -15,4 +15,11 @@ int jumlahkamar=10;
 void tampilkanDataKamar(){
     cout<<"      PROGRAM SEWA KAMAR HOTEL      "<<endl<<endl;
     for(int i=0;i<jumlahKamar;i++){
-    cout<<"Nomor Kamar : "<<hotel[i].nomorKamar<<endl
+        cout<<"Nomor Kamar : "<<hotel[i].nomorKamar<<endl;
+        cout<<"Kelas : "<<hotel[i].kelas<<endl;
+        cout<<"Kapasitas : <<hotel[i].kapasitas<<" orang"<<endl;
+        cout<<"Harga : Rp"<<hotel[i].harga<<endl;
+        cout<<"Status : "<<(hotel[i].tersedia?"Tersedia" : "Tidak Tersedia")<<endl;
+        cout<<"-------------------------------------------"<<endl;
+    }
+}
