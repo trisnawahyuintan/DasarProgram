17d03f0afede31e57745779f7a1f97927b36e861
pashkaah <137749379+pashkaah@users.noreply.github.com>
2023-07-03 15:03:11 +0700

    Update in.cpp

--- in.cpp
+++ in.cpp
@@ -17,7 +17,7 @@ void tampilkanDataKamar(){
     for(int i=0;i<jumlahKamar;i++){
         cout<<"Nomor Kamar : "<<hotel[i].nomorKamar<<endl;
         cout<<"Kelas : "<<hotel[i].kelas<<endl;
-        cout<<"Kapasitas : <<hotel[i].kapasitas<<" orang"<<endl;
+        cout<<"Kapasitas : "<<hotel[i].kapasitas<<" orang"<<endl;
         cout<<"Harga : Rp"<<hotel[i].harga<<endl;
         cout<<"Status : "<<(hotel[i].tersedia?"Tersedia" : "Tidak Tersedia")<<endl;
         cout<<"-------------------------------------------"<<endl;
